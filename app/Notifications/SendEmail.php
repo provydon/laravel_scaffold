548b3f2534e75579a6a1f96c879670a62f2da552
Providence ifeosame <providenceifeosame@gmail.com>
2021-09-29 09:59:08 +0100

    complete redis enablement

--- app/Notifications/SendEmail.php
+++ app/Notifications/SendEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\HtmlString;
 
-class SendEmail extends Notification
+class SendEmail extends Notification implements ShouldQueue
 {
     use Queueable;
 

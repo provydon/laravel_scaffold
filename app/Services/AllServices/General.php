e937bb74db643145ebb7a3e7a255f0ac31016861
Providence ifeosame <providenceifeosame@gmail.com>
2022-10-15 08:37:07 +0100

    🐛 fix test

--- app/Services/AllServices/General.php
+++ app/Services/AllServices/General.php
@@ -12,22 +12,22 @@ class General
 
     public static function logUserForTheDay(User $user)
     {
-        if (!$user->approved) {
-            throw new Exception("Account Blocked", 401);
+        if (isset($user->approved)) {
+            if (! $user->approved) {
+                throw new Exception('Account Blocked', 401);
+            }
         }
 
-
         // Log User for the day
-        date_default_timezone_set('Africa/Lagos');
-        $date = date('m/d/Y', time());
-        $dateTime = Carbon::parse($date)->toDateTimeString();
-
-        $log = $user->userLogs->where('created_at', '>=', $dateTime)->first();
-        if (!$log) {
-            if ($user->created_at < $dateTime && !$user->is_admin) {
-                $newLog = new UserLog();
-                $newLog->user_id = $user->id;
-                $newLog->save();
+        if (isset($user->userLogs)) {
+            $dateTime = now()->startOfDay()->toDateTimeString();
+            $log = $user->userLogs->where('created_at', '>=', $dateTime)->first();
+            if (! $log) {
+                if ($user->created_at < $dateTime && ! $user->is_admin) {
+                    $newLog = new UserLog();
+                    $newLog->user_id = $user->id;
+                    $newLog->save();
+                }
             }
         }
     }
